18cdc4b0ed9e3cbe59a342586573e2ff26ea647f
rajshah <rajshah.co.in@gmail.com>
2025-01-16 19:54:27 +0530

    Initial Auth Flow Test Issues Fixed

--- src/main/java/com/my_jewellers/my_jewellers/MyJewellersApplication.java
+++ src/main/java/com/my_jewellers/my_jewellers/MyJewellersApplication.java
@@ -1,13 +1,28 @@
 package com.my_jewellers.my_jewellers;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.my_jewellers.my_jewellers.role.Role;
+import com.my_jewellers.my_jewellers.role.RoleRepository;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyJewellersApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyJewellersApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findRoleByRoleName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().roleName("USER").build());
+			}
+		};
+	}
 }
