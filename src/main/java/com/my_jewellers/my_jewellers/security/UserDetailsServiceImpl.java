ccabf46198ecdef527a9a175f50f40911a875ba2
rajshah <rajshah.co.in@gmail.com>
2025-01-16 19:33:45 +0530

    Added Security & Config

---
+++ src/main/java/com/my_jewellers/my_jewellers/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.my_jewellers.my_jewellers.security;
+
+import com.my_jewellers.my_jewellers.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findUserByEmail(userEmail)
+                .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
+    }
+}
