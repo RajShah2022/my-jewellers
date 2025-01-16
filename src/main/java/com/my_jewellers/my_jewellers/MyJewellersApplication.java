16838882d2828dd6574dc03dd33f7ee357292290
rajshah <rajshah.co.in@gmail.com>
2025-01-16 19:22:13 +0530

    Added User Entity

--- src/main/java/com/my_jewellers/my_jewellers/MyJewellersApplication.java
+++ src/main/java/com/my_jewellers/my_jewellers/MyJewellersApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyJewellersApiApplication {
+public class MyJewellersApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyJewellersApiApplication.class, args);
+		SpringApplication.run(MyJewellersApplication.class, args);
 	}
 
 }
