45603bbfbf8e31afbe1a994eda2cd1a9a5b04dbf
rajshah <rajshah.co.in@gmail.com>
2025-01-16 19:30:03 +0530

    Added Token & repo

---
+++ src/main/java/com/my_jewellers/my_jewellers/user/Token.java
@@ -0,0 +1,25 @@
+package com.my_jewellers.my_jewellers.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
