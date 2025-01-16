c1aea235f9872d8200564e86c659b63bcbde6a7a
rajshah <rajshah.co.in@gmail.com>
2025-01-16 19:17:31 +0530

    Initial Commit

---
+++ src/test/java/com/my_jewellers/my_jewellers/MyJewellersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_jewellers.my_jewellers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJewellersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
